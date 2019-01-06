148551822c36b93fafb2bc265b1aa9961542b7ba
Fatlhan <45882321+Fatlhan@users.noreply.github.com>
2019-01-06 19:18:49 +0700

    Add files via upload

---
+++ CalculatorServer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.NotBoundException;
+/**
+ *
+ * @author Fatlhan Harry Chusseno
+ */
+public class CalculatorServer {
+    public static void main(String[] args) throws RemoteException,NotBoundException
+    {
+        try
+        {
+            Registry r = java.rmi.registry.LocateRegistry.createRegistry(1090);
+            r.rebind("Calculator", new Calculator() {});
+            System.out.println("server is running");
+        }
+        catch(Exception e)
+                {
+                    System.out.println(e);
+                }
+    }
+}
+
